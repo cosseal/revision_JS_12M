bbd5e7055c93439205d6a3482aabf277037a7798
cosseal <56023332+cosseal@users.noreply.github.com>
2019-12-05 13:27:33 +0100

    revisions

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+var nombre = document.getElementsByTagName("valNombre").value;
+var submit = document.getElementById("submit");
+var tableau = [nombre];
+
+submit.addEventListener("click", function () {
+    switch (submit)
+    {
+        case 10: nombre.innerHTML="";
+
+    }
+    
+})
